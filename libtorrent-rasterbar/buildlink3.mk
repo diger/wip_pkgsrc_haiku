0c3784f6595c75990159634d453c364fe1f7d95c
diger <admin@freenet59.ru>
2013-04-18 14:50:32 +0600

    update libtorrent-rasterbar

--- libtorrent-rasterbar/buildlink3.mk
+++ libtorrent-rasterbar/buildlink3.mk
@@ -5,7 +5,7 @@ BUILDLINK_TREE+=	libtorrent-rasterbar
 .if !defined(LIBTORRENT_RASTERBAR_BUILDLINK3_MK)
 LIBTORRENT_RASTERBAR_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libtorrent-rasterbar+=	libtorrent-rasterbar>=0.14.10
+BUILDLINK_API_DEPENDS.libtorrent-rasterbar+=	libtorrent-rasterbar>=0.16.4
 BUILDLINK_PKGSRCDIR.libtorrent-rasterbar?=	../../wip/libtorrent-rasterbar
 
 .endif	# LIBTORRENT_RASTERBAR_BUILDLINK3_MK
