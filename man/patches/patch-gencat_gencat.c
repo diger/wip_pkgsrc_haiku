415667c77b73450a33b55f6ba075adeb5b4eb124
diger <admin@freenet59.ru>
2013-11-19 19:17:22 +0000

    add man

--- man/patches/patch-gencat_gencat.c
+++ man/patches/patch-gencat_gencat.c
@@ -2,14 +2,12 @@ $NetBSD$
 
 --- gencat/gencat.c.orig	2006-08-02 18:11:44.000000000 +0000
 +++ gencat/gencat.c
-@@ -53,6 +53,10 @@ up-to-date.  Many thanks.
+@@ -43,7 +43,7 @@ up-to-date.  Many thanks.
+ #include <stdio.h>
+ #include <stdlib.h>
+ #include <sys/types.h>
+-#ifdef SYSV
++#if defined(SYSV) || defined(__sun)
+ #include <sys/fcntl.h>
  #include <string.h>
- #include <unistd.h>
- #endif
-+#ifdef __HAIKU__
-+#include <fcntl.h>
-+#include <string.h>
-+#endif
- #include <sys/file.h>
- #include <sys/stat.h>
- #include "gencat.h"
+ #else
