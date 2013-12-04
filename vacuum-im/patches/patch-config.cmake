faf751a9b4ac7193ee024b2ea4414509c5f2358a
diger <admin@freenet59.ru>
2013-12-04 12:42:51 +0000

    update vacuum-im & haiku patches

---
+++ vacuum-im/patches/patch-config.cmake
@@ -0,0 +1,26 @@
+$NetBSD$
+
+--- config.cmake.orig	2012-07-07 16:23:12.000000000 +0000
++++ config.cmake
+@@ -10,10 +10,6 @@ if (APPLE)
+ 	set(INSTALL_APP_DIR "vacuum")
+ 	set(INSTALL_LIB_DIR "Frameworks")
+ 	set(INSTALL_RES_DIR "Resources")
+-elseif (HAIKU)
+-	set(INSTALL_APP_DIR "Vacuum")
+-	set(INSTALL_LIB_DIR "${INSTALL_APP_DIR}/lib")
+-	set(INSTALL_RES_DIR "${INSTALL_APP_DIR}")
+ elseif (UNIX)
+ 	set(INSTALL_APP_DIR "vacuum")
+ 	set(INSTALL_LIB_DIR "lib" CACHE STRING "Name of directory for shared libraries on target system")
+@@ -34,10 +30,6 @@ elseif (APPLE)
+ 	set(PLUGINS_DIR "../PlugIns")
+ 	set(RESOURCES_DIR "../${INSTALL_RES_DIR}")
+ 	set(TRANSLATIONS_DIR "./${INSTALL_RES_DIR}/translations")
+-elseif (HAIKU)
+-	set(PLUGINS_DIR "./plugins")
+-	set(RESOURCES_DIR "./resources")
+-	set(TRANSLATIONS_DIR "./translations")
+ elseif (UNIX)
+ 	set(PLUGINS_DIR "../${INSTALL_LIB_DIR}/${INSTALL_APP_DIR}/plugins")
+ 	set(RESOURCES_DIR "../${INSTALL_RES_DIR}/${INSTALL_APP_DIR}/resources")
---
+++ vacuum-im/patches/patch-src_plugins_clientinfo_clientinfo.cpp
@@ -0,0 +1,37 @@
+$NetBSD$
+
+--- src/plugins/clientinfo/clientinfo.cpp.orig	2013-12-03 16:38:50.000000000 +0000
++++ src/plugins/clientinfo/clientinfo.cpp
+@@ -13,6 +13,7 @@
+ #include <Path.h>
+ #include <AppFileInfo.h>
+ #include <FindDirectory.h>
++extern "C" const char* __get_haiku_revision();
+ #endif
+ 
+ #define SHC_SOFTWARE_VERSION            "/iq[@type='get']/query[@xmlns='" NS_JABBER_VERSION "']"
+@@ -527,20 +528,13 @@ QString ClientInfo::osVersion() const
+ 			}
+ 		}
+ 
+-		utsname uname_info;
+-		if (uname(&uname_info) == 0) 
+-		{
+-			osver = uname_info.sysname;
+-			long revision = 0;
+-			if (sscanf(uname_info.version, "r%10ld", &revision) == 1)
+-			{
+-				char version[16];
+-				snprintf(version, sizeof(version), "%ld", revision);
++		const char* haikuRevision = __get_haiku_revision();
++		if (haikuRevision != NULL) {
++				osver = "Haiku";
+ 				osver += " ( " + strVersion + " Rev. ";
+-				osver += version;
++				osver += haikuRevision;
+ 				osver += ")";
+ 			}
+-		}
+ #else
+ 		osver = "Unknown";
+ #endif
