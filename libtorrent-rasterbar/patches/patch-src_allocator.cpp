c6d3f9dd8093da98a3e3dee579d9e850024e5fe0
diger <admin@freenet59.ru>
2014-07-03 15:18:12 +0600

    update libtorrent-rasterbar to 0.16.15 & remove broken patch

--- libtorrent-rasterbar/patches/patch-src_allocator.cpp
+++ libtorrent-rasterbar/patches/patch-src_allocator.cpp
@@ -1,25 +1,17 @@
 $NetBSD$
 
---- src/allocator.cpp.orig	2012-03-27 02:02:55.012058624 +0000
+--- src/allocator.cpp.orig	2013-09-25 07:29:17.004456448 +0000
 +++ src/allocator.cpp
-@@ -36,15 +36,12 @@ POSSIBILITY OF SUCH DAMAGE.
- 
- #ifdef TORRENT_WINDOWS
- #include <windows.h>
--#elif defined TORRENT_BEOS
--#include <kernel/OS.h>
--#include <stdlib.h> // malloc/free
- #else
- #include <stdlib.h> // valloc/free
+@@ -42,7 +42,7 @@ POSSIBILITY OF SUCH DAMAGE.
  #include <unistd.h> // _SC_PAGESIZE
  #endif
  
--#if TORRENT_USE_MEMALIGN || TORRENT_USE_POSIX_MEMALIGN
-+#if TORRENT_USE_MEMALIGN || TORRENT_USE_POSIX_MEMALIGN || defined TORRENT_HAIKU
- #include <malloc.h> // memalign
+-#if TORRENT_USE_MEMALIGN || TORRENT_USE_POSIX_MEMALIGN || defined TORRENT_WINDOWS
++#if TORRENT_USE_MEMALIGN || TORRENT_USE_POSIX_MEMALIGN || defined TORRENT_WINDOWS || defined TORRENT_HAIKU
+ #include <malloc.h> // memalign and _aligned_malloc
+ #include <stdlib.h> // _aligned_malloc on mingw
  #endif
- 
-@@ -77,8 +74,6 @@ namespace libtorrent
+@@ -86,8 +86,6 @@ namespace libtorrent
  		SYSTEM_INFO si;
  		GetSystemInfo(&si);
  		s = si.dwPageSize;
@@ -28,10 +20,10 @@ $NetBSD$
  #else
  		s = sysconf(_SC_PAGESIZE);
  #endif
-@@ -116,12 +111,6 @@ namespace libtorrent
+@@ -126,12 +124,6 @@ namespace libtorrent
  		return (char*)memalign(page_size(), bytes);
  #elif defined TORRENT_WINDOWS
- 		return (char*)VirtualAlloc(0, bytes, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
+ 		return (char*)_aligned_malloc(bytes, page_size());
 -#elif defined TORRENT_BEOS
 -		void* ret = 0;
 -		area_id id = create_area("", &ret, B_ANY_ADDRESS
@@ -40,15 +32,4 @@ $NetBSD$
 -		return (char*)ret;
  #else
  		return (char*)valloc(bytes);
- #endif
-@@ -150,10 +139,6 @@ namespace libtorrent
- 
- #ifdef TORRENT_WINDOWS
- 		VirtualFree(block, 0, MEM_RELEASE);
--#elif defined TORRENT_BEOS
--		area_id id = area_for(block);
--		if (id < B_OK) return;
--		delete_area(id);
- #else
- 		::free(block);
  #endif
